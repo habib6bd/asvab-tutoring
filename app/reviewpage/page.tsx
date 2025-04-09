2599814a445ed1c62dae51f55c86ce15398b50ca
habib6bd <habib6bd@gmail.com>
2025-04-10 04:27:29 +0600

    Fix type errors

--- app/reviewpage/page.tsx
+++ app/reviewpage/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { FaQuoteRight } from "react-icons/fa";
 
-const testimonials = [
+type Testimonial = {
+  text: string;
+  name: string;
+  image: string;
+};
+
+const testimonials: Testimonial[] = [
   {
     text: `I bought several from amazon to study the ASTB exam. None of the book has everything you need to pass the test. I also realize that all the contents in the books are very old. If you want all the materials in one place, this is the best. It has ASTB prediction test. Surprisingly, my OAR score is 5 points off of the actual oar score. my score is OAR 59 with 5,6,8. Highly recommended.`,
     name: "Hunder007",
@@ -46,37 +53,39 @@ export default function ReviewsPage() {
         <div className="text-center mb-12">
           <h2 className="text-3xl md:text-4xl text-gray-800 font-bold inline-block relative pb-4">
             Students Reviews
-            <img
+            <Image
               src="/images/border_v1.png"
               alt="underline"
-              className="absolute left-1/2 -translate-x-1/2 bottom-0 h-2 w-40 object-cover"
+              width={160}
+              height={8}
+              className="absolute left-1/2 -translate-x-1/2 bottom-0 object-cover"
             />
           </h2>
         </div>
 
         {/* Slide Container */}
         <div className="relative h-[300px] md:h-[200px] overflow-hidden">
-  {/* Previous Slide */}
-  {prevIndex !== null && isAnimating && (
-    <div
-      key={`prev-${prevIndex}`}
-      className="absolute inset-0 z-10 slide-out-left"
-    >
-      <TestimonialCard testimonial={testimonials[prevIndex]} />
-    </div>
-  )}
+          {/* Previous Slide */}
+          {prevIndex !== null && isAnimating && (
+            <div
+              key={`prev-${prevIndex}`}
+              className="absolute inset-0 z-10 slide-out-left"
+            >
+              <TestimonialCard testimonial={testimonials[prevIndex]} />
+            </div>
+          )}
 
-  {/* Current Slide */}
-  <div
-    key={`current-${currentIndex}`}
-    className={`absolute inset-0 z-20 ${
-      isAnimating ? "slide-in-right" : ""
-    }`}
-    onAnimationEnd={() => setIsAnimating(false)}
-  >
-    <TestimonialCard testimonial={testimonials[currentIndex]} />
-  </div>
-</div>
+          {/* Current Slide */}
+          <div
+            key={`current-${currentIndex}`}
+            className={`absolute inset-0 z-20 ${
+              isAnimating ? "slide-in-right" : ""
+            }`}
+            onAnimationEnd={() => setIsAnimating(false)}
+          >
+            <TestimonialCard testimonial={testimonials[currentIndex]} />
+          </div>
+        </div>
 
         {/* Dots */}
         <div className="flex justify-center mt-6 space-x-2">
@@ -94,16 +103,18 @@ export default function ReviewsPage() {
   );
 }
 
-function TestimonialCard({ testimonial }: { testimonial: any }) {
+function TestimonialCard({ testimonial }: { testimonial: Testimonial }) {
   return (
     <div className="p-4 md:px-12 text-center">
       <FaQuoteRight className="text-5xl text-yellow-500 mx-auto mb-4" />
       <p className="text-base md:text-lg font-medium">{testimonial.text}</p>
       <div className="flex justify-center items-center mt-6 gap-3">
-        <img
+        <Image
           src={testimonial.image}
           alt={testimonial.name}
-          className="w-10 h-10 rounded-full object-cover"
+          width={40}
+          height={40}
+          className="rounded-full object-cover"
         />
         <span className="font-semibold">{testimonial.name}</span>
       </div>
